851d4e979f19e77e425348e70e7aa1c6c45ccab0
Everton José Marinho de Araujo <juninhoxd6@hotmail.com>
2018-04-08 15:20:03 -0300

    Update connection.js

--- api/connection.js
+++ api/connection.js
@@ -1,10 +1,10 @@
 var connectionString = {
-    host: '', // server name or IP address;
-    port: 5432,
-    database: '', // Database name
-    user: '', // Database user
-    password: '' // Database password
-  };
+  host: 'ec2-54-204-21-226.compute-1.amazonaws.com', // server name or IP address;
+  port: 5432,
+  database: 'd5psijb3oto6ql',
+  user: 'anzkuobymqywis',
+  password: '9b1907260f99912b4d03ae6d3d3d0ff940c9cf3da8225175ebcf792867e63f5d'
+};
 
   function returnConnection() {
       return connectionString;
